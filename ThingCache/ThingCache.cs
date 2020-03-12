716e149c2264ce3e0f6b13769e03800458b463cf
Chazov Maxim <maximud.00@gmail.com>
2020-03-12 16:36:52 +0300

    Add Tests

--- ThingCache/ThingCache.cs
+++ ThingCache/ThingCache.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using FakeItEasy;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace MockFramework
@@ -7,6 +9,7 @@ namespace MockFramework
     {
         private readonly IDictionary<string, Thing> dictionary
             = new Dictionary<string, Thing>();
+
         private readonly IThingService thingService;
 
         public ThingCache(IThingService thingService)
@@ -24,6 +27,7 @@ namespace MockFramework
                 dictionary[thingId] = thing;
                 return thing;
             }
+
             return null;
         }
     }
@@ -43,11 +47,86 @@ namespace MockFramework
         [SetUp]
         public void SetUp()
         {
-            //thingService = A...
+            thingService = A.Fake<IThingService>();
             thingCache = new ThingCache(thingService);
         }
 
-        //TODO: написать простейший тест, а затем все остальные
-        //Live Template tt работает!
+        [TearDown]
+        public void TearDown()
+        {
+            thingService = null;
+            thingCache = null;
+        }
+
+        [Test]
+        public void ReturnCorrectThing_TakeItFirstTimeFromService()
+        {
+            A.CallTo(() => thingService.TryRead(thingId1, out thing1)).Returns(true);
+            
+            thingCache.Get(thingId1).Should().Be(thing1);
+            A.CallTo(() => thingService.TryRead(thingId1, out thing1)).MustHaveHappened();
+        }
+
+        [Test]
+        public void Get_NonExistingObject_ReturnsNull()
+        {
+            thingCache.Get(thingId1).Should().BeNull();
+            A.CallTo(() => thingService.TryRead(thingId1, out thing1)).MustHaveHappened();
+        }
+
+        [Test]
+        public void DoestInvokeServiceMethod_OnSecondTime()
+        {
+            A.CallTo(() => thingService.TryRead(thingId1, out thing1)).Returns(true);
+            thingCache.Get(thingId1);
+            thingCache.Get(thingId1);
+            A.CallTo(() => thingService.TryRead(thingId1, out thing1)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Test]
+        public void ReturnNull_OnEmptyString_IfNotExistEqualId()
+        {
+            thingCache.Get("").Should().BeNull();
+        }
+
+        [Test]
+        public void ReturnCorrectThing_OnMoreThanOneCaches()
+        {
+            A.CallTo(() => thingService.TryRead(thingId1, out thing1)).Returns(true);
+            A.CallTo(() => thingService.TryRead(thingId2, out thing2)).Returns(true);
+            thingCache.Get(thingId1).Should().Be(thing1);
+            thingCache.Get(thingId2).Should().Be(thing2);
+        }
+
+        [Test]
+        public void ReturnZero_OnNonExistId()
+        {
+            const string someId = "srgdfgh";
+            thingCache.Get(someId).Should().Be(null);
+            Thing nullThing = null;
+            A.CallTo(() => thingService.TryRead(someId, out nullThing)).MustHaveHappened(Repeated.Exactly.Once);
+
+        }
+
+        [Test]
+        public void ReturnSameThings_OnMoreThanOneGetMethodInvoke()
+        {
+            A.CallTo(() => thingService.TryRead(thingId1, out thing1)).Returns(true);
+            var thing = thingCache.Get(thingId1);
+            thingCache.Get(thingId1).Should().Be(thing);
+        }
+
+        [Test]
+        public void SupportsNullThing_IfIdExists()
+        {
+            const string id = "esd";
+            Thing thing = null;
+            A.CallTo(() => thingService.TryRead(id, out thing)).Returns(true);
+
+            thingCache.Get(id).Should().BeNull();
+            thingCache.Get(id).Should().BeNull();
+
+            A.CallTo(() => thingService.TryRead(id, out thing)).MustHaveHappened(Repeated.Exactly.Once);
+        }
     }
 }
